85c2e0ffb8a5e859c2865a71a15bf3e4d0624760
Your Name <you@example.com>
2021-11-02 10:29:39 +0100

    Add project files.

---
+++ RolePlayingAbilityPrototyping/Participants/IParticipant.cs
@@ -0,0 +1,16 @@
+﻿using RolePlayingAbilityPrototyping.Spells;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RolePlayingAbilityPrototyping.Participants
+{
+    public interface IParticipant
+    {
+        string Name { get; }
+        double Modifier { get; }
+        int Level { get; }
+        Dictionary<SpellType, double> SpellVector { get; }
+        double NotPresentValue { get; }
+    }
+}
---
+++ RolePlayingAbilityPrototyping/Participants/ParticipantBase.cs
@@ -0,0 +1,42 @@
+﻿using RolePlayingAbilityPrototyping.Spells;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RolePlayingAbilityPrototyping.Participants
+{
+    public abstract class ParticipantBase : IParticipant, IEnumerable<Tuple<SpellType, double>>
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public Dictionary<SpellType, double> SpellVector { get; set; }
+        public abstract double Modifier { get; set; }
+        public abstract double NotPresentValue { get; set; }
+
+        protected ParticipantBase(string name, int level)
+        {
+            Name = name;
+            Level = level;
+            SpellVector = new Dictionary<SpellType, double>();
+        }
+
+        public double this[SpellType spellType]
+        {
+            get { return SpellVector[spellType] != 0 ? SpellVector[spellType] : NotPresentValue; }
+        }
+
+        public IEnumerator<Tuple<SpellType, double>> GetEnumerator()
+        {
+            foreach (var spellType in Spell.SpellTypeList)
+            {
+                yield return new Tuple<SpellType, double>(spellType, this[spellType] * Modifier);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
